c3d3131e8c951ccee667626b3908897781a90635
Bridget Asser <bridgetasser@gmail.com>
2020-02-16 16:34:51 -0500

    connecting to backend api and setting up handling signup from front end

--- src/user/Register.jsx
+++ src/user/Register.jsx
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Layout from "../core/Layout";
+import { API } from "../config";
 
-const Register = () => (
-    <Layout
-        title="Register"
-        description="Register to The Public Lobby App"
-    ></Layout>
-);
+const Register = () => {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false
+    });
+
+    const { name, email, password, success, error } = values;
+
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    };
+
+    const registerForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input
+                    onChange={handleChange("name")}
+                    type="text"
+                    className="form-control"
+                    value={name}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input
+                    onChange={handleChange("email")}
+                    type="email"
+                    className="form-control"
+                    value={email}
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input
+                    onChange={handleChange("password")}
+                    type="password"
+                    className="form-control"
+                    value={password}
+                />
+            </div>
+        </form>
+    );
+
+    return (
+        <Layout
+            title="Register"
+            description="Register to The Public Lobby"
+            className="container col-md-8 offset-md-2"
+        >
+            {registerForm()}
+            {JSON.stringify(values)}
+        </Layout>
+    );
+};
 
 export default Register;
