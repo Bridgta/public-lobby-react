7d38a63c083ad1c520fe99eefde29a373adf4ea3
Bridget Asser <bridgetasser@gmail.com>
2020-02-16 13:45:01 -0500

    work on nav bar and active buttons with history

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,18 @@
 import React from "react";
-
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./components/user/Register";
 import Signin from "./components/user/Login";
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
+import Home from "./core/Home";
+import Menu from "./core/Menu";
 
 const Routes = () => {
     return (
         <BrowserRouter>
+            <Menu />
             <Switch>
+                <Route path="/" exact component={Home} />
                 <Route path="/login" exact component={Login} />
                 <Route path="/register" exact component={Register} />
             </Switch>
