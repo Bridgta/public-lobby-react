dbcc12248b3488a9d5009dc02e53f854c65f7342
Bridget Asser <bridgetasser@gmail.com>
2020-02-17 12:02:45 -0500

    api for pshowing project info and new from the provate route

---
+++ src/core/Card.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+
+const Card = ({ project }) => {
+    return (
+        <div className="card ">
+            <div className="card-header card-header-1 ">{project.title}</div>
+            <div className="card-body">
+                <p>{project.description}</p>
+                <p>${project.amountNeeded}</p>
+                <Link to="/">
+                    {" "}
+                    <button className="btn.btn-outline-primary mt-2 mb-2">
+                        view project
+                    </button>
+                    <button className="btn.btn-outline-primary mt-2 mb-2">
+                        Donate Now
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- src/core/Home.jsx
+++ src/core/Home.jsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProjects } from "./apiCore";
+import Card from "./Card";
 
-const Home = () => (
-    <Layout title="Home" description="The Public Lobby App"></Layout>
-);
+const Home = () => {
+    const [projectsBySell, setProjectsBySell] = useState([]);
+    const [projectsByArrival, setProjectsByArrival] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadProjectsBySell = () => {
+        getProjects("donated").then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProjectsBySell(data);
+            }
+        });
+    };
+
+    const loadProjectsByArrival = () => {
+        getProjects("createdAt").then(data => {
+            console.log(data);
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProjectsByArrival(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadProjectsByArrival();
+        loadProjectsBySell();
+    }, []);
+
+    return (
+        <Layout
+            title="The Public Lobby"
+            description="The Public Lobby"
+            className="container-fluid"
+        >
+            <h2 className="mb-4">New Bills</h2>
+            <div className="row">
+                {projectsByArrival.map((project, i) => (
+                    <div key={i} className="col-4 mb-3">
+                        <Card project={project} />
+                    </div>
+                ))}
+            </div>
+
+            <h2 className="mb-4">Trending Funds</h2>
+            <div className="row">
+                {projectsBySell.map((project, i) => (
+                    <div key={i} className="col-4 mb-3">
+                        <Card project={project} />
+                    </div>
+                ))}
+            </div>
+        </Layout>
+    );
+};
 
 export default Home;
